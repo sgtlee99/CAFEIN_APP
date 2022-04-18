3e73f4862e7c252c022bcae4e66ec59c5583d0d0
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-04-18 11:00:54 +0900

    최초화면(Origin) 로그인화면(Login) 회원가입(Signup) 설문조사(Survey) 홈화면(Home) 추가 -승준

--- app/src/main/java/com/example/cafein_app/OriginActivity.kt
+++ app/src/main/java/com/example/cafein_app/OriginActivity.kt
@@ -12,11 +12,12 @@ class OriginActivity : AppCompatActivity() {
 
         var loginbutton = findViewById<Button>(R.id.originpage_loginbutton)
         var signupbutton = findViewById<Button>(R.id.originpage_siginupbutton)
-
+        //로그인 버튼 이벤트
         loginbutton.setOnClickListener {
             var intent = Intent(this, LoginActivity::class.java) // 오리진화면 -> 로그인
             startActivity(intent)
         }
+        //회원가입 버튼 이벤트
         signupbutton.setOnClickListener {
             var intent = Intent(this, SignupActivity::class.java) // 오리진 화면 -> 회원가입
             startActivity(intent)
--- app/src/main/java/com/example/cafein_app/SignupActivity.kt
+++ app/src/main/java/com/example/cafein_app/SignupActivity.kt
@@ -1,11 +1,20 @@
 package com.example.cafein_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup_activity)
+
+        var signupnextbutton = findViewById<Button>(R.id.signuppage_NextButton)
+
+        signupnextbutton.setOnClickListener {
+            var intent = Intent(this, SurveyActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
