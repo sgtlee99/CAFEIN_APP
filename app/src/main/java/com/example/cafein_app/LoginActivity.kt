e382e1f847f30eb25bf86e287d89adaa3f21f833
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-05-02 10:17:02 +0900

    플래그먼트 구현

--- app/src/main/java/com/example/cafein_app/LoginActivity.kt
+++ app/src/main/java/com/example/cafein_app/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.cafein_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        var loginbutton = findViewById<Button>(R.id.loginpage_loginbutton)
+        loginbutton.setOnClickListener {
+            var intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
