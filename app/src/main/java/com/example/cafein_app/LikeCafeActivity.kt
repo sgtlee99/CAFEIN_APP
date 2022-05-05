292ef79067a6705a1d83e61778a64f547a92ec49
DESKTOP-PHRLLH5\USER <gnsdlrkt35@naver.com>
2022-05-06 01:04:05 +0900

    mypage myinfochange likecafe ui

---
+++ app/src/main/java/com/example/cafein_app/LikeCafeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cafein_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LikeCafeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.like_cafe_activity)
+    }
+}
---
+++ app/src/main/java/com/example/cafein_app/MyInfoChangeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cafein_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyInfoChangeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_info_change_activity)
+    }
+}
