f06ba75a0198d463a1482b75713ca66dd78b7268
DESKTOP-PHRLLH5\USER <gnsdlrkt35@naver.com>
2022-05-08 21:10:00 +0900

    mypage myinfochange likecafe mywrittenboard ui
    fragment ok

--- app/src/main/java/com/example/cafein_app/SettingFragment.kt
+++ app/src/main/java/com/example/cafein_app/SettingFragment.kt
@@ -19,6 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SettingFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class SettingFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -41,13 +42,31 @@ class SettingFragment : Fragment() {
             var intent = Intent(context, MyInfoChangeActivity::class.java)
             startActivity(intent)
         }
+
+        var likecafe = view?.findViewById<Button>(R.id.mypage_LikeCafeButton)
+        likecafe?.setOnClickListener {
+            var intent = Intent(context, LikeCafeActivity::class.java)
+            startActivity(intent)
+        }
+
+        var myboard = view?.findViewById<Button>(R.id.mypage_MyBoardButton)
+        myboard?.setOnClickListener {
+            var intent = Intent(context, MyWrittenBoardActivity::class.java)
+            startActivity(intent)
+        }
+
+        var logout = view?.findViewById<Button>(R.id.mypage_LogoutButton)
+        logout?.setOnClickListener {
+            var intent = Intent(context, OriginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
