00fd2042741b858859316774f6c1caf45ae483fd
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-05-12 14:26:42 +0900

    구조수정 1단계 준비 test2

--- app/src/main/java/com/example/cafein_app/HomeActivity.kt
+++ app/src/main/java/com/example/cafein_app/HomeActivity.kt
@@ -19,7 +19,7 @@ class HomeActivity : AppCompatActivity() {
 
         // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
         var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
-
+//d
         // OnNavigationItemSelectedListener를 통해 탭 아이템 선택 시 이벤트를 처리
         // navi_menu.xml 에서 설정했던 각 아이템들의 id를 통해 알맞은 프래그먼트로 변경하게 한다.
         bnv_main.run {
