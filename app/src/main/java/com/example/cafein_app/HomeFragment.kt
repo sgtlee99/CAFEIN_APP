2be09d74db05f973d80ab352f459556f50437ea0
DESKTOP-PHRLLH5\USER <gnsdlrkt35@naver.com>
2022-05-27 01:07:02 +0900

    boardreview test
    no finish

--- app/src/main/java/com/example/cafein_app/HomeFragment.kt
+++ app/src/main/java/com/example/cafein_app/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.cafein_app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
+import android.widget.Button
 import android.widget.ListView
 import androidx.annotation.Nullable
 import androidx.recyclerview.widget.RecyclerView
