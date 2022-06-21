4c52f98d62752ca39c881a2e2eb0128b0b61a705
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-06-21 16:49:57 +0900

    글자 위치 조금 수정

--- app/src/main/java/com/example/cafein_app/HomeActivity.kt
+++ app/src/main/java/com/example/cafein_app/HomeActivity.kt
@@ -3,20 +3,25 @@ package com.example.cafein_app
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        return super.onCreateOptionsMenu(menu)
-        val menuInflater = menuInflater
-        menuInflater.inflate(R.menu.home_appbar, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
+//        val menuInflater = menuInflater
+//        menuInflater.inflate(R.menu.home_appbar, menu)
+//        return true
+
+
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
+
+        var action_bar : ActionBar = supportActionBar!!
 
         // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
         var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
@@ -30,14 +35,20 @@ class HomeActivity : AppCompatActivity() {
                         // 다른 프래그먼트 화면으로 이동하는 기능
                         val homeFragment = HomeFragment()
                         supportFragmentManager.beginTransaction().replace(R.id.fl_container, homeFragment).commit()
+                        action_bar.setTitle("CAFE-IN")
+
                     }
                     R.id.second -> {
                         val writingFragment = WritingFragment()
                         supportFragmentManager.beginTransaction().replace(R.id.fl_container, writingFragment).commit()
+                        action_bar.setTitle("글쓰기")
+
                     }
                     R.id.third -> {
                         val settingFragment = SettingFragment()
                         supportFragmentManager.beginTransaction().replace(R.id.fl_container, settingFragment).commit()
+                        action_bar.setTitle("설정")
+
                     }
                 }
                 true
--- app/src/main/java/com/example/cafein_app/SplashActivity.kt
+++ app/src/main/java/com/example/cafein_app/SplashActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    val SPLASH_VIEW_TIME: Long = 2000  // 2초간 스플래시 화면을 보여줌(ms)
+    val SPLASH_VIEW_TIME: Long = 200  // 2초간 스플래시 화면을 보여줌(ms)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
