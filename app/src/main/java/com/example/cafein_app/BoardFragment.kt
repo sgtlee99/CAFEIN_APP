434f553df0446b5dc357c3a2be4d22bc7e61d3fa
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-06-22 13:27:17 +0900

    좋아요 버튼 만듬

--- app/src/main/java/com/example/cafein_app/BoardFragment.kt
+++ app/src/main/java/com/example/cafein_app/BoardFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.fragment_boartview.*
 
 class BoardFragment : Fragment() {
+
+    var likePost : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,26 @@ class BoardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_boartview, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        var likebutton : ImageButton = view!!.findViewById(R.id.post_likebutton)
+        likebutton.setOnClickListener {
+            if(likebutton.isSelected) {
+                likebutton.setImageResource(R.drawable.likebuttonunselect)
+                likePost = true
+            } else {
+                likebutton.setImageResource(R.drawable.likebuttonselect)
+                likePost = false
+            }
+            likebutton.setSelected(!likebutton.isSelected())
+        }
+
     }
 }
