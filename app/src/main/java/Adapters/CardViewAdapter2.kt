90ff0a7755a8c7ef9867402b26150c43e803c2e0
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-06-24 10:38:03 +0900

    624 좋아요 한 카페 추가

---
+++ app/src/main/java/Adapters/CardViewAdapter2.kt
@@ -0,0 +1,66 @@
+package com.example.myapplication1
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cafein_app.R
+import java.util.ArrayList
+
+
+class CardViewAdapter2(val itemList2: ArrayList<CardView_Info>): RecyclerView.Adapter<CardViewAdapter2.ViewHolder>() {
+
+    // 아이템 레이아웃과 결합
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewAdapter2.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.board_card_view, parent, false)
+        return ViewHolder(view)
+    }
+    // 리스트 내 아이템 개수
+    override fun getItemCount(): Int {
+        return itemList2.size
+    }
+    // View에 내용 입력
+    override fun onBindViewHolder(holder: CardViewAdapter2.ViewHolder, position: Int) {
+        holder.Title.text = itemList2[position].title
+        holder.Tag.text = itemList2[position].tag
+        holder.Image.setImageResource(itemList2[position].image)
+        holder.G1.text = itemList2[position].g1.toString()
+        holder.G2.text = itemList2[position].g2.toString()
+        holder.G3.text = itemList2[position].g3.toString()
+        holder.G4.text = itemList2[position].g4.toString()
+
+        // 리스트 내 항목 클릭 시 onClick() 호출
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
+    }
+
+    // (4) 레이아웃 내 View 연결
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        val Title : TextView = itemView.findViewById(R.id.cardview_title)
+        val Tag : TextView= itemView.findViewById(R.id.cardview_tag)
+        val Image : ImageView = itemView.findViewById(R.id.cardview_cafe_img)
+        val G1 : AppCompatButton= itemView.findViewById(R.id.cardview_group1_button)
+        val G2 : AppCompatButton= itemView.findViewById(R.id.cardview_group2_button)
+        val G3 : AppCompatButton= itemView.findViewById(R.id.cardview_group3_button)
+        val G4 : AppCompatButton= itemView.findViewById(R.id.cardview_group4_button)
+
+    }
+    //리스너 인터페이스
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+
+    //외부에서 클릭시 이벤트 설정
+    fun setItemClickListener(onItemClickListener: CardViewAdapter2.OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+    // setItemClickListener로 설정한 함수 실행
+    private lateinit var itemClickListener: CardViewAdapter2.OnItemClickListener
+}
--- app/src/main/java/com/example/cafein_app/LikeCafeActivity.kt
+++ app/src/main/java/com/example/cafein_app/LikeCafeActivity.kt
@@ -3,12 +3,41 @@ package com.example.cafein_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.ActionBar
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cafein_app.databinding.LikeCafeActivityBinding
+import com.example.myapplication1.CardViewAdapter
+import com.example.myapplication1.CardViewAdapter2
+import com.example.myapplication1.CardView_Info
 
 class LikeCafeActivity : AppCompatActivity() {
+
+    private lateinit var binding : LikeCafeActivityBinding
+    val itemList2 = arrayListOf<CardView_Info>()
+    val like_postlist = CardViewAdapter2(itemList2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.like_cafe_activity)
+        binding = LikeCafeActivityBinding.inflate(layoutInflater)
+//        setContentView(R.layout.like_cafe_activity)
+        setContentView(binding.root)
 
+        //레이아웃 매니저와 어댑터 설정
+        binding.likeCafeList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.likeCafeList.adapter=like_postlist
 
+        itemList2.add(CardView_Info("영진카페", "#복현동 #아메리카노", R.drawable.cafe_front, 3, 2, 5, 4))
+        itemList2.add(CardView_Info("로켓", "#태그1 #태그2", R.drawable.nasa1, 44, 0, 8, 6))
+        itemList2.add(CardView_Info("자동차", "#무슨사진 #이더라", R.drawable.beatle1, 50, 5, 5, 12))
+        itemList2.add(CardView_Info("카페테스트2", "#tag1 #tag2", R.drawable.cafe_front3, 78, 9, 5, 99))
+
+        like_postlist.notifyDataSetChanged()
+
+        //타이틀 수정
+        Appbar_Setting()
+    }
+    private fun Appbar_Setting() {
+        var search_actionbar : ActionBar = supportActionBar!!
+        search_actionbar.setTitle("좋아요 한 카페")
     }
 }
