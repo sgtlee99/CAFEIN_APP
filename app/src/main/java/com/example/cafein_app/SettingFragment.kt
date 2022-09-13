386d598532e5b9951f52038e956262d9ef0b1a87
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-09-13 16:24:23 +0900

    아몰랑 8월 마지막날

--- app/src/main/java/com/example/cafein_app/SettingFragment.kt
+++ app/src/main/java/com/example/cafein_app/SettingFragment.kt
@@ -2,6 +2,7 @@ package com.example.cafein_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -34,11 +35,12 @@ class SettingFragment : Fragment() {
             startActivity(intent)
         }
     }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_setting, container, false)
+        val id_val: String? = arguments?.getString("id_data")
+        Log.d("SHOW_TEST", "아이디 setting으로 받아옴 : $id_val")
+
+        return view
     }
 }
